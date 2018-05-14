f7bbe52c28eb8bf7d8b10dfa475839f5cca44190
senthil.alagarsamy <senthil.alagarsamy@hcl.com>
2018-05-14 19:48:31 +0530

    Commit all proper files

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+    
+	@Test
+	public void SimpleTest()
+	{
+	 int a = 1;
+	 int b = 4;
+	 assertTrue(a + b == 5);
+	}
+}
